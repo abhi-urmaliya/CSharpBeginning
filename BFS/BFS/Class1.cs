311cbe2400f8be95f385e223172aec5c06bf89dc
abhi-urmaliya <abhilash.urmaliya@gmail.com>
2014-05-26 12:26:07 +0530

    First Commit
    First ever commit

---
+++ BFS/BFS/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFS
+{
+    class Item
+    {
+        public void search(int[,] a, bool[] v, int n)
+        {
+            var BFSqueue = new Queue<int>();
+            BFSqueue.Enqueue(0);
+            v[0] = true;
+            /*for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    Console.Write("{0}, ", a[i,j].ToString());
+                }
+            }*/
+            bool flag = true;
+            int k;
+            while (true)
+            {
+                k = BFSqueue.Dequeue();
+                Console.Write("{0}, ", k.ToString());
+                for (int j = 0; j < n; j++)
+                {
+                    //Console.Write("Enqueue loop entered.\n");
+                    if (a[k, j] == 1 && v[j] == false && BFSqueue.Contains(j) == false)
+                    {
+                        BFSqueue.Enqueue(j);
+                        //Console.Write("{0} enqueued!\n", j.ToString());
+                    }
+                }
+                v[k] = true;
+                for (int i = 0; i < n; i++)
+                {
+                    flag &= v[i];
+                    //Console.Write(flag.ToString());
+                }
+                if (!flag)
+                {
+                    continue;
+                }
+                else
+                    break;
+            }
+        }
+    }
+}
---
+++ BFS/BFS/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] G = new int[5, 5];
+            for (int i = 0; i < 5; i++) {
+                for (int j = i; j < 5; j++) {
+                    G[i, j] = G[j, i] = int.Parse(Console.ReadLine());
+                }
+            }
+            var visited = new bool[5];
+            for (int i = 0; i < 5; i++) {
+                visited[i] = false;
+            }
+            var s = new Item();
+            s.search(G, visited, 5);
+        }
+    }
+}
